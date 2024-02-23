4fa904d00aedf4766d6a8632ba4708f8eade4a54
Vishal Nair <148544787+vishalnair2002@users.noreply.github.com>
2024-02-23 15:08:40 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25-pound bag of <em>irresistible</em>, organic goodness for your cat",
+        price: 2000,
+        image: "images/product-fullsize.png",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1002,
+        title: "Yarn",
+        description: "A 25-pound bag of <em>irresistible</em>, organic goodness for your cat",
+        price: 500,
+        image: "images/product-fullsize.png",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1003,
+        title: "Kitty Litter",
+        description: "Thomas Cat Litter is the suitable choice for cats that are equally picky and discerning.",
+        price: 1000,
+        image: "images/Kitty Litter.jpg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1004,
+        title: "Cat Scratching Post",
+        description: "This Gluckluz cat scratch post is easy to assemble and disassemble, taking only 3 steps, less than 5 minutes to install.",
+        price: 2500,
+        image: "images/Cat Scratching Post.jpg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1005,
+        title: "Cat Bed for Indoor Cats",
+        description: "🍩 With a triangular entrance and cave-like design, 2-in-1 enclosed pet tent bed functions, keeping them feel safe and sheltered.",
+        price: 1200,
+        image: "images/product-fullsize.png",
+        availableInventory: 10,
+        rating: 3
+    }
+];
