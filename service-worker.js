b8e27cc5f7609533c79c4a872404aafe99e3634b
Vishal Nair <148544787+vishalnair2002@users.noreply.github.com>
2024-02-23 15:54:20 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,7 @@ e.waitUntil(
     })
    );
 }); 
+
 self.addEventListener('install', (e) => {
 console.log('[Service Worker] Install');
 e.waitUntil(
@@ -22,4 +23,19 @@ e.waitUntil(
       return cache.addAll(cacheFiles);
     })
    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
